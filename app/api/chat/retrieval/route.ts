89ce446237054d063fa400978efb76143bc785dd
Matheus S Alves <matheus.souza.alves@gmail.com>
2024-02-09 03:51:53 -0300

    style:  change colors, add younner logo

--- app/api/chat/retrieval/route.ts
+++ app/api/chat/retrieval/route.ts
@@ -45,9 +45,9 @@ const condenseQuestionPrompt = PromptTemplate.fromTemplate(
   CONDENSE_QUESTION_TEMPLATE,
 );
 
-const ANSWER_TEMPLATE = `Your purpose is to answer questions about Younner, a technology company. Use a friendly, joyful and informative tone and provide accurate information. You must talk about Younner and its products and services like a proud employee. 
+const ANSWER_TEMPLATE = `Your purpose is to answer questions about Younner, a technology company. Use a friendly, joyful and informative tone and provide accurate information. You must talk about Younner, its products and services like a proud employee. 
 Try not to use the source information exactly as it is. Instead, use your own words to explain the information.
-.You should never sound like you are guessing or unsure of the answer.
+You should never sound like you are guessing or unsure of the answer.
 
 Also consider the following context and chat history:
 <context>
